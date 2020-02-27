f49aacf548f7e424f51b487d15a2da1aa4fce677
wakatabi_tomoki <wakatabi_tomoki@cyberagent.co.jp>
2020-02-27 20:46:45 +0900

    initial commit with balloon comoponent

---
+++ src/components/atoms/balloon/index.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Balloon from "./index";
+
+export default (stories) =>
+  stories
+    .add("2文字ラベル", () => <Balloon>次へ</Balloon>)
+    .add("4文字ラベル", () => <Balloon>削除する</Balloon>)
+    .add("絶対配置", () => (
+      <Balloon style={{ position: "absolute", top: "200px", left: "200px" }}>
+        左上から200pxに配置
+      </Balloon>
+    ));

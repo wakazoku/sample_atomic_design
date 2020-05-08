60b3b3d6cb08c3539c0deda1419f5366a6397a1e
zoku <>
2020-05-09 01:15:04 +0900

    アイコンコンポーネント追加

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -1,23 +1,44 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import styles from "./style.css";
 
-export const TrashCanIcon = ({
-  height = 20,
-  width = 20,
-  className = "",
+export const IconContainer = ({
+  presenter,
   onClick,
+  className = "",
   ...props
 }) => {
   if (onClick) className += `${styles.clickable}`;
+  return presenter({ onClick, className, ...props });
+};
+
+export const IconPresenter = ({
+  iconName,
+  height = 20,
+  width = 20,
+  ...props
+}) => (
+  <img
+    src={`/icons/${iconName}.svg`}
+    alt=""
+    height={height}
+    width={width}
+    {...props}
+  />
+);
+
+export const iconFactory = (iconName) => (props) => {
+  console.log(iconName);
   return (
-    <img
-      src="/icons/trash-can.svg"
-      alt=""
-      height={height}
-      width={width}
-      className={className}
-      onClick={onClick}
-      {...props}
+    <IconContainer
+      presenter={(presenterProps) => <IconPresenter {...presenterProps} />}
+      {...{ iconName, ...props }}
     />
   );
 };
+
+export const TrashCanIcon = iconFactory("trash-can");
+export const ChevronRightIcon = iconFactory("chevron-right");
+export const SearchIcon = iconFactory("search");
+export const SettingIcon = iconFactory("settings");
+
--- src/components/atoms/Icon/index.stories.js
+++ src/components/atoms/Icon/index.stories.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
-import { TrashCanIcon } from "./index";
+import {
+  TrashCanIcon,
+  ChevronRightIcon,
+  SearchIcon,
+  SettingIcon,
+} from "./index";
 
 export default (stories) =>
   stories
     .add("TrashCanIcon", () => <TrashCanIcon />)
     .add("クリッカブル", () => (
       <TrashCanIcon onClick={action("アイコンがクリックされました")} />
-    ));
+    ))
+    .add("矢印", () => <ChevronRightIcon />)
+    .add("検索", () => <SearchIcon />)
+    .add("設定", () => <SettingIcon />);

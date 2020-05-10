bf77d50a469ba3b8233657c97587f7cc8d938572
wakazoku <wakazoku@gmail.com>
2020-05-11 01:56:35 +0900

    molecules層、DeleteButtonコンポーネント追加

---
+++ src/components/molecules/DeleteButton/index.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import DeleteButton from "./index";
+import { withStyle } from "../../utils/decorator";
+
+export default (stories) =>
+  stories.add("デフォルト", () =>
+    withStyle({ margin: "50px" })(
+      <DeleteButton onClick={action("削除ボタンがクリックされました")} />
+    )
+  );
